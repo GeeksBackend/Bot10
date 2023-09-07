db277f17148318ffdf84e34e597ac4abddef679e
Toktorov <ktoktorov2005@gmail.com>
2023-09-07 19:49:39 +0600

    add inline buttons

--- tiktok_bot.py
+++ tiktok_bot.py
@@ -1,18 +1,44 @@
 from aiogram import Bot, Dispatcher, types, executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher.storage import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
 from config import token 
 import os, time, logging, requests, random
 
 bot = Bot(token=token)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
+
+inline_buttons = [
+    types.InlineKeyboardButton('Пройти идентификацию', callback_data='identification'),
+    types.InlineKeyboardButton('Информация о видео тик ток', callback_data='tiktok_info')
+]
+inline_keyboard = types.InlineKeyboardMarkup().add(*inline_buttons)
 
 @dp.message_handler(commands='start')
 async def start(message:types.Message):
-    await message.answer(f"Привет {message.from_user.full_name}")
+    await message.answer(f"Привет {message.from_user.full_name}", reply_markup=inline_keyboard)
+
+identification_buttons = [
+    types.KeyboardButton('Отправить номер', request_contact=True),
+    types.KeyboardButton('Отправить локацию', request_location=True)
+]
+identification_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(*identification_buttons)
+
+@dp.message_handler(commands='identification')
+async def check_identification(message:types.Message):
+    await message.answer("Отправьте свой номер телефона для идентификации", reply_markup=identification_keyboard)
+
+@dp.callback_query_handler(lambda call: call)
+async def all_inline_buttons(call):
+    if call.data == "identification":
+        print("Кнопка работает")
+        await check_identification(call.message)
+
+@dp.message_handler(content_types=types.ContentType.CONTACT)
+async def get_number(message:types.Message):
+    await message.answer(f'{message.contact.phone_number}')
+
+@dp.message_handler(content_types=types.ContentTypes.LOCATION)
+async def get_location(message:types.Message):
+    await message.answer(f'Долгота: {message.location.latitude}, широта: {message.location.longitude}')
 
 @dp.message_handler()
 async def download_send_video(message:types.Message):
@@ -38,5 +64,6 @@ async def download_send_video(message:types.Message):
                 await message.answer_video(send_file)
         except Exception as error:
             await message.answer(f"Ошибка: {error}")
+        os.remove(f'video/{title_video}.mp4')
 
 executor.start_polling(dp, skip_updates=True)
